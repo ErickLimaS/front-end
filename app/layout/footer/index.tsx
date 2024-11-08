c9a97a14cc7e91371f3dc09cc6a74d48c907d336
Erick Lima <ericklima63@gmail.com>
2024-11-07 22:07:28 -0300

    refactor: Add Link on Footer
    Updated Footer component to include a link to Erick Lima's GitHub profile

--- app/layout/footer/index.tsx
+++ app/layout/footer/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return <footer className="bg-blue-900">Footer</footer>;
+  return (
+    <footer className="bg-blue-900 py-8 px-4">
+      <small className="flex justify-center whitespace-pre text-base">
+        Made with ❤️ by{" "}
+        <Link
+          className="underline "
+          href={"https://github.com/ErickLimaS/"}
+          target="_blank"
+        >
+          Erick Lima
+        </Link>
+      </small>
+    </footer>
+  );
 }
 
 export default Footer;
